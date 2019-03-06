2b90f0819a37f1c5ebf92fb01225bc9e5f3daf55
Codey Notestine <codeynotestine@gmail.com>
2019-03-05 22:55:29 -0500

    Lab6 End

---
+++ web/src/app/books/books/books.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../books/book';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+  books: Book[] = [
+  {
+    id: 1,
+    title:'Ready Player One',
+    author: 'Earnest Cline',
+  },
+  {
+    id: 2,
+    title: 'Catch 22',
+    author: 'Josheph Heller'
+  }
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
