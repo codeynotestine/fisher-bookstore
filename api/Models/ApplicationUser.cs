a1e55bf34b14aee305153f44daebf00b11df1db5
Codey Notestine <codeynotestine@gmail.com>
2019-04-10 18:55:27 -0400

    Lab10

---
+++ api/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Fisher.Bookstore.Models;
+
+
+namespace Fisher.Bookstore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [NotMapped]
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        
+    }
+
+}
